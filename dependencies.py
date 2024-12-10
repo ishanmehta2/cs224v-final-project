332e2df408c0bd219841411bb6941e38329898d7
ishanmehta2 <121464699+ishanmehta2@users.noreply.github.com>
2024-12-09 17:08:55 -0800

    Create dependencies.py

---
+++ dependencies.py
@@ -0,0 +1,20 @@
+# INSTALL AND IMPORT THESE PACKAGES/LIBRARIES BEFORE RUNNING THE PROGRAM
+
+import requests
+import openai
+import os
+import base64
+from io import BytesIO
+from typing import List, Tuple
+from difflib import SequenceMatcher
+import sacrebleu
+from collections import Counter
+import json
+import numpy as np
+import pandas as pd 
+import string
+import pytesseract
+from pytesseract import Output
+import cv2
+from PIL import Image
+import os
